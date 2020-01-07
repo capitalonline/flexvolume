0a6251f7f509749f0dfbdcb98844ceb9a0f15b4e
sui jizhi <jizhi.sui@capitalonline.net>
2020-01-07 10:46:25 +0800

    add cdscloud~nas driver deploy scripts

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+cd ${GOPATH}/src/github.com/CapitalonlineService/flexvolume/
+#GIT_SHA=`git rev-parse --short HEAD || echo "HEAD"`
+
+
+export GOARCH="amd64"
+export GOOS="linux"
+if [[ "$(uname -s)" == "Linux" ]];then
+	CGO_ENABLED=1 go build -tags 'netgo' --ldflags '-extldflags "-static"' -o flexvolume-linux 
+else
+	CGO_ENABLED=0 go build -o flexvolume-linux 
+fi
+
