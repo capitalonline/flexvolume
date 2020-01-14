dd1f20d667f9f3fd8545f5c32eb0abe223c1df86
Honglin Feng <tifayuki@gmail.com>
2020-01-14 09:41:55 +0800

    make go build use vendor folder

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ cd ${GOPATH}/src/github.com/capitalonline/flexvolume/
 export GOARCH="amd64"
 export GOOS="linux"
 if [[ "$(uname -s)" == "Linux" ]];then
-	CGO_ENABLED=1 go build -tags 'netgo' --ldflags '-extldflags "-static"' -o flexvolume-linux 
+	CGO_ENABLED=1 go build -tags 'netgo' --ldflags '-extldflags "-static"' -o flexvolume-linux -mod=vendor -v
 else
-	CGO_ENABLED=0 go build -o flexvolume-linux 
+	CGO_ENABLED=0 go build -o flexvolume-linux -mod=vendor -v
 fi
 
